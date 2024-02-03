26234881f2b4fdf67b5cd756ef8e41402ea5ee21
alarbon <laboa1998@gmail.com>
2024-02-03 19:00:02 +0100

    mongo conexion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PalpediaModule } from './palpedia/palpedia.module';
@@ -11,7 +9,7 @@ import { PalpediaModule } from './palpedia/palpedia.module';
     MongooseModule.forRoot(process.env.MONGO_URL),
     PalpediaModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
